61b0d37efb636ca39402ee35046c5b932eaa7276
walid <walidksouri42@gmail.com>
2024-07-05 23:13:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const successMsg='<i class="fa-solid fa-circle-check"></i>Successfully sumbmitted';
+const errorMsg='<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
+const invalidMsg='<i class="fa-solid fa-circle-exclamation"></i>Invalid input,check again';
+const toast=document.getElementById('toastBox');
+    function showToast(msg){
+        const toast=document.createElement('div');
+        toast.classList.add('toast');
+        toast.innerHTML=msg;
+        toastBox.appendChild(toast);
+        if(msg.includes('error')){
+            toast.classList.add('error');
+        }
+        else if(msg.includes('Invalid')){
+            toast.classList.add("Invalid");
+        }
+        setTimeout(()=>{
+            toast.remove();
+        },6000);
+    }
+//git push <remote-name> <local-branch-name>:<remote-branch-name>
+//git push origin main:development
