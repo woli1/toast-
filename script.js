b176c4812fb449835aaca92dffda855867334d5b
walid <walidksouri42@gmail.com>
2024-07-06 00:29:07 +0100

    commit

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const toast=document.getElementById('toastBox');
             toast.remove();
         },6000);
         console.log('ali');
-        ////
+        /////
         
     }
 //git push <remote-name> <local-branch-name>:<remote-branch-name>
